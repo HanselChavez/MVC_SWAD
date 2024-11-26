c5d690743bf4243c989ef5904b9cc281e0436bc6
HanselChavez <n00274924@upn.pe>
2024-11-26 17:07:11 -0500

    CSP

---
+++ src/main/java/Security/ContentSecurityFilter.java
@@ -0,0 +1,30 @@
+package Security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.FilterConfig;
+
+@WebFilter(filterName = "ContentSecurityFilter", urlPatterns = {"/*"})
+public class ContentSecurityFilter extends HttpFilter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Inicialización si es necesario
+    }
+
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        httpResponse.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' https://apis.google.com;");
+
+        chain.doFilter(request, response);
+    }
+}
