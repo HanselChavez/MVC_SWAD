2645b05a2f8810d646c7cd48074cb6b6c78a70ba
HanselChavez <n00274924@upn.pe>
2024-11-26 17:44:11 -0500

    git

--- src/main/java/Security/CSPFilter.java
+++ src/main/java/Security/CSPFilter.java
@@ -24,20 +24,15 @@ public class CSPFilter extends HttpFilter {
 
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-        // Aquí estamos configurando las directivas CSP para permitir los scripts de los CDN necesarios
         String cspPolicy = "default-src 'self'; "
                 + "script-src 'self' 'unsafe-inline' https://apis.google.com https://code.jquery.com https://cdn.datatables.net https://cdn.jsdelivr.net https://cdn.tailwindcss.com https://cdn.jsdelivr.net/npm/toastify-js https://www.google.com https://www.gstatic.com; "
                 + "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://cdn.tailwindcss.com https://cdnjs.cloudflare.com https://fonts.googleapis.com https://cdn.datatables.net; "
                 + "font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com; "
                 + "frame-src 'self' https://www.google.com https://www.gstatic.com; "
                 + "img-src 'self' https://cdn.datatables.net;";
-        
-        
-        
-        
+
         httpResponse.setHeader("Content-Security-Policy", cspPolicy);
 
-        // Continuar con el siguiente filtro o la respuesta
         chain.doFilter(request, response);
     }
 }
